30ed550ba027e2a24250acdbf659afbafd0644ea
alizein245-hash <ali.zein245@gmail.com>
2026-09-24 16:06:28 +0200

    Update aphrody_test.js

--- aphrody_test.js
+++ aphrody_test.js
@@ -18,29 +18,19 @@ console.log();
 try {
   const result = await handleAnyFutUrl(playerUrl);
 
-  console.log("ERGEBNIS:");
+  console.log();
+  console.log("=".repeat(70));
+  console.log("ROHES ERGEBNIS");
+  console.log("=".repeat(70));
+  console.log();
+
   console.log(JSON.stringify(result, null, 2));
 
   console.log();
   console.log("=".repeat(70));
-  console.log("PREIS-TEST");
+  console.log("TEST ABGESCHLOSSEN");
   console.log("=".repeat(70));
 
-  if (result?.data?.prices) {
-    console.log("Preisdaten:");
-    console.log(JSON.stringify(result.data.prices, null, 2));
-
-    console.log();
-    console.log("PlayStation / Console:");
-
-    if (result.data.prices.console !== undefined) {
-      console.log(result.data.prices.console);
-    } else {
-      console.log("Kein console-Preis gefunden.");
-    }
-  } else {
-    console.log("Keine Preisdaten im Ergebnis gefunden.");
-  }
 } catch (error) {
   console.log();
   console.log("=".repeat(70));
